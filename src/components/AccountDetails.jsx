319bd7afebd5661cb9bf9eac708c32e2f98daa87
AyushKumarThakur21BCE3771 <144116323+AyushKumarThakur21BCE3771@users.noreply.github.com>
2024-11-08 06:32:02 +0530

    first commit

--- src/components/AccountDetails.jsx
+++ src/components/AccountDetails.jsx
@@ -78,7 +78,7 @@ const Card = () => {
       ) : (
         <div className="group before:hover:scale-95 before:hover:w-80 before:hover:h-40 before:hover:rounded-b-2xl before:transition-all before:duration-500 before:content-[''] before:w-80 before:h-24 before:rounded-t-2xl before:bg-gradient-to-bl from-sky-200 via-orange-200 to-orange-700 before:absolute before:top-0 w-80 h-90 relative bg-slate-50 flex flex-col items-center justify-center gap-2 text-center rounded-2xl overflow-hidden">
           <div className="w-28 h-28 bg-[#e49287] mt-8 rounded-full border-4 border-slate-50 z-10 group-hover:scale-[70%] group-hover:-translate-x-24 group-hover:-translate-y-[30px] transition-all duration-500">
-            <img src="https://avatar.iran.liara.run/public" />
+            <img src="https://i.ibb.co/RTKjrQq/rb-4704.png" />
           </div>
           <div className="z-10 group-hover:-translate-y-[60px] transition-all duration-500">
             <span className="text-2xl font-semibold">
